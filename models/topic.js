9a421d8e1edbabc11198c18331daba3bb1f7367f
mdnazmul42726 <nazmulofficial@outlook.com>
2024-02-07 23:51:02 +0600

    The data model of the topic is created

---
+++ models/topic.js
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from 'mongoose';
+
+const topicSchema = new Schema(
+    {
+        title: String,
+        description: String,
+    },
+    {
+        timestamps: true
+    }
+)
+
+const topic = mongoose.models.Topic || mongoose.model('Topic', topicSchema);
+
+export default Topic;
