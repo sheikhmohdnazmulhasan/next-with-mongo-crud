28c22ddbac1ad51f643a41e6d5f268e96a545c57
mdnazmul42726 <nazmulofficial@outlook.com>
2024-02-08 22:12:08 +0600

    All the topics are fetched from the database and mapped to the client site

--- components/TopicList.js
+++ components/TopicList.js
@@ -3,20 +3,40 @@ import React from 'react';
 import { CiEdit } from "react-icons/ci";
 import { MdDelete } from "react-icons/md";
 
+const getTopics = async () => {
+    try {
+
+        const res = await fetch('http://localhost:3000/api/topics', { cache: 'no-store' });
+
+        if (!res.ok) {
+            throw new Error('Failed to fetch topics');
+        }
+
+        return res.json();
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const TopicList = async () => {
+
+    const { topics } = await getTopics();
 
 
-const TopicList = () => {
     return (
-        <div className='flex justify-between border p-4 border-slate-700 items-start gap-5 my-2'>
-            <div className="">
-                <h2 className='text-xl font-bold'>Topic Title</h2>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium, eius.</p>
-            </div>
-            <div className="flex gap-4">
-                <Link href={'/edit-topic/123'}><CiEdit size={24} /></Link>
-                <MdDelete size={24} />
-            </div>
-        </div>
+        <>
+            {topics.map((t) => <div key={t._id} className='flex justify-between border p-4 border-slate-700 items-start gap-5 my-2'>
+                <div className="">
+                    <h2 className='text-xl font-bold'>{t?.title}</h2>
+                    <p>{t?.description}</p>
+                </div>
+                <div className="flex gap-4">
+                    <Link href={'/edit-topic/123'}><CiEdit size={24} /></Link>
+                    <MdDelete size={24} />
+                </div>
+            </div>)}
+        </>
     );
 };
 
